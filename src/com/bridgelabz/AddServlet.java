6d4bfc1dedb421ae91e82e414760c5a8e710414a
pranav <prnvkatkar88@gmail.com>
2022-01-19 10:38:57 +0530

    Http session used

--- src/com/bridgelabz/AddServlet.java
+++ src/com/bridgelabz/AddServlet.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet {
 	
@@ -16,6 +17,10 @@ public class AddServlet extends HttpServlet {
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
+		
+		HttpSession session = req.getSession();
+		session.setAttribute("k", k);
+		
 		res.sendRedirect("sq?k=" +k);
 		
 //		RequestDispatcher rd = req.getRequestDispatcher("sq");
--- src/com/bridgelabz/SqServlet.java
+++ src/com/bridgelabz/SqServlet.java
@@ -6,12 +6,16 @@ import java.io.PrintWriter;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
 		
-		int k = Integer.parseInt(req.getParameter("k"));
-		k = k*k;
+		HttpSession session = req.getSession();
+		
+		int k = (int) session.getAttribute("k");
+		k = k * k;
+		
 		
 		PrintWriter out = res.getWriter();
 		out.println("hello sq res is" +k);
