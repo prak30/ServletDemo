9b48c847254b9cbd4602d2e4bc5795059802340d
pranav <prnvkatkar88@gmail.com>
2022-01-19 12:08:51 +0530

    here we have changed body color by using servlet. in a servlet we write html code inside java

--- src/com/bridgelabz/AddServlet.java
+++ src/com/bridgelabz/AddServlet.java
@@ -21,10 +21,10 @@ public class AddServlet extends HttpServlet {
 
 		int k = i + j;
 
-		Cookie cookie = new Cookie("k", k + "");
-		res.addCookie(cookie);
-
-		res.sendRedirect("sq");
+		PrintWriter out = res.getWriter();
+		out.println("<html><body bgcolor='cyan'>");
+		out.println("output:" +k);
+		out.println("</body></html>");
 
 		// RequestDispatcher rd = req.getRequestDispatcher("sq");
 		// rd.forward(req,res);
