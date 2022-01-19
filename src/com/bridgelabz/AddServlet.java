e8adb9854e04b10f692ab43c76404278ce434eaa
pranav <prnvkatkar88@gmail.com>
2022-01-19 11:01:14 +0530

    cookies implemented

--- src/com/bridgelabz/AddServlet.java
+++ src/com/bridgelabz/AddServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -18,10 +19,10 @@ public class AddServlet extends HttpServlet {
 		
 		int k = i + j;
 		
-		HttpSession session = req.getSession();
-		session.setAttribute("k", k);
+		Cookie cookie = new Cookie("k", k + "");
+		res.addCookie(cookie);
 		
-		res.sendRedirect("sq?k=" +k);
+		res.sendRedirect("sq");
 		
 //		RequestDispatcher rd = req.getRequestDispatcher("sq");
 //		rd.forward(req,res);
--- src/com/bridgelabz/SqServlet.java
+++ src/com/bridgelabz/SqServlet.java
@@ -3,6 +3,7 @@ package com.bridgelabz;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,10 +11,15 @@ import javax.servlet.http.HttpSession;
 
 public class SqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
+		int k = 0;
+		Cookie cookies[] = req.getCookies();
 		
-		HttpSession session = req.getSession();
+		for(Cookie c :cookies){
+			if(c.getName().equals("k"))
+				k=Integer.parseInt(c.getValue());
+		}
 		
-		int k = (int) session.getAttribute("k");
+	
 		k = k * k;
 		
 		
